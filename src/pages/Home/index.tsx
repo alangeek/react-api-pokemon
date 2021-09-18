a81fad830362b9155954052a4e677bec0c9a0563
Alan Christian <alanchristiancreator@hotmail.com>
2021-09-17 22:28:55 -0300

    feat(Home): create interfaces for Pokemon and API response

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,16 +4,29 @@ import PokemonItem from "../../components/PokemonItem";
 import PokemonLogo from "../../assets/images/logo1.png";
 import styles from './styles.module.css'
 import api from "../../services/api";
-// import api from "../../services/api";
+
+interface IPokemon {
+  name: string
+  url: string
+}
+
+interface IResponse {
+  count: number;
+  next: string | null
+  previous: string | null
+  results: IPokemon[]
+}
 
 function Home() {
-  const [pokemons, setPokemons] = useState()
+  const [pokemons, setPokemons] = useState<IPokemon[]>()
   
   const getAllPokemons = async () => {
-    const { data } = await api.get('pokemon')
+    const { data } = await api.get<IResponse>('pokemon')
     
-    setPokemons(data)
+    setPokemons(data.results)
   }
+
+  console.log({ pokemons })
   
   return (
     <div className={styles.container}>
